e7f4c208f8d644b37326b2bf026cdececc9486e5
oqbaowda <oqba2010@oqbaowda.(none)>
2010-03-25 14:37:11 +0200

    fix url eol

--- apps/groupmessaging/urls.py
+++ apps/groupmessaging/urls.py
@@ -9,8 +9,8 @@ from views import groups
 from views import recipients
 
 urlpatterns = patterns('',
-    url(r'groupmessaging/?', index.index, name='index'),
-    url(r'groupmessaging/messages?', messages.list, name='messages_list'),
-    url(r'groupmessaging/groups?', groups.list, name='manage_groups'),
-    url(r'groupmessaging/recipients?', recipients.list, name='recipients_list')
+    url(r'^groupmessaging/?$', index.index, name='index'),
+    url(r'^groupmessaging/messages/?$', messages.list, name='messages_list'),
+    url(r'^groupmessaging/groups/?$', groups.list, name='manage_groups'),
+    url(r'^groupmessaging/recipients/?$', recipients.list, name='recipients_list')
 )
