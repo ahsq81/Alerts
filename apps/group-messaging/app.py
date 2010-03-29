3a87292104a34c2d48401152b9b0fb69a6235e0c
reg <reg@devsms.(none)>
2010-03-29 10:47:42 +0300

    Added group-messaging app

---
+++ apps/group-messaging/app.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+import rapidsms
+
+class App(rapidsms.app.App):
+    pass
---
+++ apps/group-messaging/models.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# encoding=utf8
+
+from django.db import models
+
+
