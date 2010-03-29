46a1a30de4fd3f61889cbdc42b3b048c26434521
reg <reg@devsms.(none)>
2010-03-29 11:13:40 +0300

    Site Model

--- apps/group-messaging/models.py
+++ apps/group-messaging/models.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 # encoding=utf8
 
+''' Group Messaging Models '''
+
 from django.db import models
 
 
+class Site(models.Model):
+    ''' Site Model
+
+    Stores Top Level Entity
+    Sites holds groups and users and recipients '''
+
+    name = models.CharField(max_length=50)
+    status = models.BooleanField()
+    credit = models.PositiveIntegerField()
+    manager = models.ForeignKey("User", blank=True, null=True)
+
+    def __unicode__(self):
+        return _(u"%s") % self.name
