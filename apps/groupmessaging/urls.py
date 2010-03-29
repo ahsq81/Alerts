c6c45303f7c14ac208ab01be1b5a0012a27d9fa0
Mohannad Zalloom <mzalloom@mohannad.(none)>
2010-03-29 18:41:47 +0300

    Messages Template added

--- apps/groupmessaging/urls.py
+++ apps/groupmessaging/urls.py
@@ -4,7 +4,9 @@
 from django.conf.urls.defaults import *
 
 from views import index
+from views import messages
 
 urlpatterns = patterns('',
     url(r'groupmessaging/?', index.index, name='index'),
+    url(r'groupmessaging/messages?', messages.list, name='messages_list'),
 )
---
+++ apps/groupmessaging/views/messages.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+from rapidsms.webui.utils import render_to_response
+
+def list(request):
+    return render_to_response(request, 'messages.html', {})
