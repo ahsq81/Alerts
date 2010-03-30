d1dda00f2e0c500702c2ead43ec01c2c6e12ddb4
reg <reg@devsms.(none)>
2010-03-30 11:13:22 +0300

    updated index UI

--- apps/groupmessaging/views/index.py
+++ apps/groupmessaging/views/index.py
@@ -4,11 +4,14 @@
 from rapidsms.webui.utils import render_to_response
 
 from groupmessaging.views.common import webuser_required
+from groupmessaging.models import SendingLog
 
 
 @webuser_required
 def index(request, context):
 
-    mycontext = {}
+    latest_messages = SendingLog.objects.all()[:5]
+
+    mycontext = {'latest_messages': latest_messages}
     context.update(mycontext)
     return render_to_response(request, 'index.html', context)
