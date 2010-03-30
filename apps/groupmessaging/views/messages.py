78d1613a93f0eb4d14f9af9773d31f496809245d
Mohannad Zalloom <mzalloom@mohannad.(none)>
2010-03-30 15:46:21 +0300

    form started

--- apps/groupmessaging/views/messages.py
+++ apps/groupmessaging/views/messages.py
@@ -19,20 +19,33 @@ def list(request, context):
     return render_to_response(request, 'messages.html', context)
 
 @webuser_required
-def messageform(request, context, messageid):
+def messageform(request, context, messageid=None):
     '''form for Add/Edit messages'''
     
-    if request.method == 'POST':
-    
-        form = MessageForm(request.POST)
-        if form.is_valid(): 
-            
-            return HttpResponseRedirect('/thanks/') 
-    else:
+    if not messageid or int(messageid) == 0:
+        mess = None
+    else :
         mess = Message.objects.get(id=messageid)
-        data = {'name': mess.name, 'text': mess.text, 'code':mess.code}
-        form = MessageForm(data) 
         
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            print form.cleaned_data['code']
+            mess.code =  form.cleaned_data['code']
+            mess.name =  form.cleaned_data['name']
+            mess.text =  form.cleaned_data['text']
+            mess.save()
+            
+    else:
+        if mess :
+            data = {'name': mess.name, 'text': mess.text, 'code':mess.code}
+            form = MessageForm(data) 
+        else:
+            data = {}
+            
+    if not messageid:
+        messageid=0
+    
     mycontext = {'mess':mess, 'form':form}
     context.update(mycontext)
     return render_to_response(request,"messages_form.html",context)
@@ -42,5 +55,6 @@ class MessageForm(forms.Form):
   
     code = forms.CharField(max_length=20)
     name = forms.CharField(max_length=50)
-    text = forms.CharField()
+    text = forms.CharField(widget=forms.Textarea(),initial="Please enter your message here")
+
     
