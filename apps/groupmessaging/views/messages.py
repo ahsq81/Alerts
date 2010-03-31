77465d79e81deda0ee383eadc12e7197ceaf0de5
Mohannad Zalloom <mzalloom@mohannad.(none)>
2010-03-31 11:36:28 +0300

    Add Messages Working

--- apps/groupmessaging/views/messages.py
+++ apps/groupmessaging/views/messages.py
@@ -6,12 +6,14 @@ from groupmessaging.views.common import webuser_required
 from django import forms
 from django.shortcuts import redirect
 from groupmessaging.models import Message
+from groupmessaging.models import Site
 
 @webuser_required
 def list(request, context):
     
     messages = Message.objects.all()
-    mycontext = {'messages':messages}
+    count = Message.objects.count()
+    mycontext = {'messages':messages, 'count':count}
     context.update(mycontext)
 
     return render_to_response(request, 'messages.html', context)
@@ -28,22 +30,35 @@ def messageform(request, context, messageid=None):
     if request.method == 'POST':
         form = MessageForm(request.POST)
         if form.is_valid():
-            mess.code =  form.cleaned_data['code']
-            mess.name =  form.cleaned_data['name']
-            mess.text =  form.cleaned_data['text']
-            mess.save()
-            
+            if mess :
+                mess.code =  form.cleaned_data['code']
+                mess.name =  form.cleaned_data['name']
+                mess.text =  form.cleaned_data['text']
+                mess.site =  context['user'].site
+                mess.save()
+                
+            else :
+                mess = Message(code = form.cleaned_data['code'] , name = form.cleaned_data['name'], \
+                                           text = form.cleaned_data['text'], \
+                                           site = context['user'].site)  
+                mess.save()
+                
+                mess = Message.objects.all()
+                mycontext = {'mess':mess}
+                context.update(mycontext)
+                return render_to_response(request, 'messages.html', context)
+           
     else:
         if mess :
-            data = {'name': mess.name, 'text': mess.text, 'code':mess.code}
-            form = MessageForm(data) 
+            data = {'name': mess.name, 'text': mess.text, 'code':mess.code}          
         else:
-            data = {}
+            data = {'code': 'code','name':'name','test':'text'}
+        form = MessageForm(data) 
             
     if not messageid:
         messageid=0
     
-    mycontext = {'mess':mess, 'form':form}
+    mycontext = {'mess':mess, 'form':form , 'messageid':messageid}
     context.update(mycontext)
     return render_to_response(request,"messages_form.html",context)
 
@@ -65,5 +80,6 @@ class MessageForm(forms.Form):
     code = forms.CharField(max_length=20)
     name = forms.CharField(max_length=50)
     text = forms.CharField(widget=forms.Textarea(),initial="Please enter your message here")
+
 
     
